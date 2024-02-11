93aba586914cd8fcce70f8bc2f0871fe1b699576
Priyanshu Vaish <106699296+Priyansuvaish@users.noreply.github.com>
2024-02-11 14:15:15 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -4,47 +4,40 @@ import java.util.Scanner;
   {
     public static void main(String args[])
      {
-       float a, b, res;
+       double a, b, res;
        char choice, ch;
        Scanner scan = new Scanner(System.in);
 	      
       	do
          {
 	   System.out.print("Calculator demo\n");
-           System.out.print("1. Addition\n");
-           System.out.print("2. Subtraction\n");
-           System.out.print("3. Multiplication\n");
-           System.out.print("4. Division\n");
+           System.out.print("1. Square Root \n");
+           System.out.print("2. Fortorial\n");
+           System.out.print("3. Power\n");
+           System.out.print("4. Log\n");
            System.out.print("5. Exit\n\n");
            System.out.print("Enter Your Choice : ");
            choice = scan.next().charAt(0);
            switch(choice)
             {
-              case '1' : System.out.print("Enter Two Number : ");
-                         a = scan.nextFloat();
-                         b = scan.nextFloat();
-                         res = a + b;
-                         System.out.print("Result = " + res);
+              case '1' : System.out.print("Enter the number for square root : ");
+                         a = scanner.nextDouble();
+                    	 System.out.println("Result: " + Math.sqrt(a));
 	                 break;															                       
-	      case '2' : System.out.print("Enter Two Number : ");
-	                 a = scan.nextFloat();
-	                 b = scan.nextFloat();
-	                 res = a - b;
-	                 System.out.print("Result = " + res);
+	      case '2' : System.out.print("Enter the number for factorial : ");
+	                 a = scanner.nextDouble();
+                    	 System.out.println("Result: " + factorial((int)x));
 	                 break;
 	 
-	      case '3' : System.out.print("Enter Two Number : ");
-	                 a = scan.nextFloat();
-	                 b = scan.nextFloat();
-	                 res = a * b;
-	                 System.out.print("Result = " + res);
+	      case '3' : System.out.print("Enter the number for natural log : ");
+	                 a = scanner.nextDouble();
+                    	 System.out.println("Result: " + Math.log(x));
 	                 break;
 	 
-	      case '4' : System.out.print("Enter Two Number : ");
-	                 a = scan.nextFloat();
-	                 b = scan.nextFloat();
-	                 res = a / b;
-	                 System.out.print("Result = " + res);
+	      case '4' : System.out.print("Enter two number for power \n Note: 2 3 mean the 2 power 3 that is 8 : ");
+	                 a = scan.nextDouble();
+	                 b = scan.nextDouble();
+	                 System.out.println("Result: " + Math.pow(x, b));
 	                 break;
 	 
 	      case '5' : System.exit(0);
@@ -55,5 +48,12 @@ import java.util.Scanner;
 	   }
 				System.out.print("\n---------------------------------------\n");
 	}while(choice != 5);       
+    }
+public static long factorial(int number) {
+        long result = 1;
+        for (int factor = 2; factor <= number; factor++) {
+            result *= factor;
+        }
+        return result;
     }
 }
